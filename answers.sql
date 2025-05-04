c2980cca2169d91b412b3fe9e0c65a03d5647ed1
Nompumelelo <nompumelelopeninamthembu@gmail.com>
2025-05-04 16:04:57 +0200

    3rd commit

--- answers.sql
+++ answers.sql
@@ -1,26 +1,17 @@
 -- 1.
-WITH RECURSIVE split_products AS (
-  SELECT
-    OrderID,
-    CustomerName,
-    TRIM(SUBSTRING_INDEX(Products, ',', 1)) AS Product,
-    SUBSTRING(Products, LENGTH(SUBSTRING_INDEX(Products, ',', 1)) + 2) AS Remaining
-  FROM RawOrders
+CREATE TABLE ProductDetail (
+OrderId INT,
+CustomerName VARCHAR(100),
+Product VARCHAR(100),
+);
 
-  UNION ALL
-
-  SELECT
-    OrderID,
-    CustomerName,
-    TRIM(SUBSTRING_INDEX(Remaining, ',', 1)),
-    SUBSTRING(Remaining, LENGTH(SUBSTRING_INDEX(Remaining, ',', 1)) + 2)
-  FROM split_products
-  WHERE Remaining != ''
-)
-
-SELECT OrderID, CustomerName, Product
-FROM split_products
-ORDER BY OrderID;
+INSERT INTO ProductDetail(OrderID, CustomerName, Product) VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 
 
@@ -28,24 +19,6 @@ ORDER BY OrderID;
 
 
 --2.
-
-
-
-CREATE TABLE OrderDetails (
-    OrderID INT,
-    CustomerName VARCHAR(100),
-    Product VARCHAR(100),
-    Quantity INT
-);
-
-INSERT INTO OrderDetails VALUES
-(101, 'John Doe', 'Laptop', 2);
-(101, 'John Doe', 'Mouse', 1);
-(102, 'Jane Smith', 'Tablet', 3);
-(102, 'Jane Smith', 'Keyboard', 1);
-(102, 'Jane Smith', 'Mouse', 2);
-(103, 'Emily Clark', 'Phone', 1);
-
 
 
 CREATE TABLE Orders (
@@ -60,7 +33,7 @@ INSERT INTO Orders (OrderID, CustomerName) VALUES
 
 
 
-CREATE TABLE OrderItems (
+CREATE TABLE Product (
     OrderID INT,
     Product VARCHAR(100),
     Quantity INT,
@@ -69,7 +42,7 @@ CREATE TABLE OrderItems (
 );
 
 
-INSERT INTO OrderItems (OrderID, Product, Quantity) VALUES
+INSERT INTO Product (OrderID, Product, Quantity) VALUES
 (101, 'Laptop', 2);
 (101, 'Mouse', 1);
 (102, 'Tablet', 3);
